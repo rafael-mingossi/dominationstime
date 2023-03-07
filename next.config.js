4e35caa3248adf3867efc889d80e7bb21cc521ee
rafael-mingossi <rafaelmingossi@gmail.com>
2023-03-07 14:02:27 +1100

    initial set up

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
+const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  output: 'standalone',
+  trailingSlash: false,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'tools/styles/base')],
+    prependData: '@import "resources.scss";'
+  },
+
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      ...defaultPathMap
+    };
+  },
+  webpack: (config, options) => {
+    // SVG
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack']
+    });
+
+    return config;
+  }
+};
+
+module.exports = nextConfig;
